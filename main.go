cd40d1fac3ee45d4e7971f048373e796371bb584
Alistair Ferguson <alistair.ferguson@polestarglobal.com>
2019-07-15 10:03:28 +0100

    Tidy up and increase minimum sleep time to one minute

--- main.go
+++ main.go
@@ -28,14 +28,35 @@ var imos = []string{
 	"9261011",
 	"9353802",
 }
-var num_imos = len(imos)
+var numImos = len(imos)
 
 const workers = 10
 const url = "https://purpletrac.polestar-testing.com/api/v1/registration?api_key=0d5ad56b6de8cfb14f81232aaae3d2543f9313ed&username=alistair.ferguson%40polestarglobal.com"
 
-func pick_a_ship() string {
-	i := rand.Intn(num_imos)
+func pickAShip() string {
+	i := rand.Intn(numImos)
 	return imos[i]
+}
+
+func registerAShip(sigs chan int, ship string) {
+	time.Sleep(time.Duration(60+rand.Intn(120)) * time.Second)
+	body, _ := json.Marshal(map[string]string{
+		"registered_name":  ship,
+		"client_reference": "altest autogen",
+	})
+
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		log.Fatalln(err)
+		sigs <- 1
+	}
+	resp.Body.Close()
+	if resp.StatusCode != 201 {
+		log.Fatalln(resp.Status)
+		sigs <- 1
+	}
+
+	fmt.Println("Registered ship " + ship)
 }
 
 func main() {
@@ -44,25 +65,7 @@ func main() {
 	for i := 0; i < workers; i++ {
 		go func() {
 			for {
-				time.Sleep(time.Duration(rand.Intn(60)) * time.Second)
-				ship := pick_a_ship()
-				body, _ := json.Marshal(map[string]string{
-					"registered_name":  ship,
-					"client_reference": "altest autogen",
-				})
-
-				resp, err := http.Post(url, "application/json", bytes.NewBuffer(body))
-				if err != nil {
-					log.Fatalln(err)
-					sigs <- 1
-				}
-				resp.Body.Close()
-				if resp.StatusCode != 201 {
-					log.Fatalln(resp.Status)
-					sigs <- 1
-				}
-
-				fmt.Println("Registered ship " + ship)
+				registerAShip(sigs, pickAShip())
 			}
 		}()
 	}
